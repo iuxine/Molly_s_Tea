b04b75dec578d233d99ee778fa5c35581897efc7
iuxine <yuxinl030909@gmail.com>
2026-04-22 20:38:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function filterMenu(type, event) {
+function filter(type, event) {
   const cards = document.querySelectorAll(".card");
-  const buttons = document.querySelectorAll(".filters button");
+  const buttons = document.querySelectorAll(".nav button");
 
   buttons.forEach(b => b.classList.remove("active"));
-  event.target.classList.add("active");
+  if (event) event.target.classList.add("active");
 
   cards.forEach(card => {
     if (type === "all" || card.classList.contains(type)) {
